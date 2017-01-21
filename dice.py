1c0dbad9e0aa87cb4eebb8af1a6dc26963cca147
fapable <ramonvanek@outlook.com>
2017-01-21 22:34:37 +0100

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,16 @@
+import random
+
+uitkomst = random.randrange(1, 6)
+
+if uitkomst == 1:
+    print("gegooide cijfer is 1")
+if uitkomst == 2:
+    print("gegooide cijfer is 2")
+if uitkomst == 3:
+    print("gegooide cijfer is 3")
+if uitkomst == 4:
+    print("gegooide cijfer is 4")
+if uitkomst == 5:
+    print("gegooide cijfer is 5")
+if uitkomst == 6:
+    print("gegooide cijfer is 6")
